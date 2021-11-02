91618b12d4e9f5dde0406a6bcdfb4b04a602b0a4
KOTerra <82443072+KOTerra@users.noreply.github.com>
2021-11-02 19:16:24 +0200

    merge in visual studio tileson

---
+++ Mape/Mapgenerator/MapGen/MapGen.cpp
@@ -0,0 +1,51 @@
+#include <fstream>
+#include <iostream>
+#include <Windows.h>
+#include <commdlg.h>
+#include <sstream>
+#include <cstddef> 
+#include <tileson.h>
+
+namespace fisier {
+	std::string fisierFolosit;
+	bool fisierIncarcat = false;
+}
+
+void openFile()
+{
+
+	OPENFILENAMEA ofn;
+	CHAR szFile[260] = { 0 };
+	CHAR currentDir[256] = { 0 };
+	ZeroMemory(&ofn, sizeof(OPENFILENAME));
+	ofn.lStructSize = sizeof(OPENFILENAME);
+	ofn.lpstrFile = szFile;
+	ofn.nMaxFile = sizeof(szFile);
+
+	if (GetCurrentDirectoryA(256, currentDir)) {
+		ofn.lpstrInitialDir = currentDir;
+	}
+
+	ofn.nFilterIndex = 1;
+	ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
+
+
+
+	if (GetOpenFileNameA(&ofn) == TRUE) {
+		fisier::fisierFolosit = ofn.lpstrFile;
+		fisier::fisierIncarcat = true;
+		return;
+
+	}
+
+	fisier::fisierIncarcat = false;
+
+
+}
+int main() {
+	openFile();
+
+
+	std::cout << fisier::fisierIncarcat << "\n " << fisier::fisierFolosit;
+	return 0;
+}
