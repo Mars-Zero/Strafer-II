af56f76f6394a86324771de95278834903c082ff
KOTerra <82443072+KOTerra@users.noreply.github.com>
2021-11-30 21:10:55 +0200

    Vector

--- StraferII/Vector2d.java
+++ StraferII/Vector2d.java
@@ -27,6 +27,17 @@ public class Vector2d
         float v1 = b.y - a.y;
         return Math.sqrt(v0*v0 + v1*v1);
     }
+
+        public float distance(float x, float y)
+	{
+		//Finds the x and y distance between both vectors by subtracting their x and y components. Note which vector is subtracted by which other vector is irrelevent,
+		//since the magnitude of the resulting vector will be the same.
+		float distX = this.x - x;
+		float distY = this.y - y;
+		
+		//Returns the magnitude of the distance between both vectors. This is done via the pythagorean theorem, using sqrt(x*x + y*y).
+		return (float)Math.sqrt(distX*distX + distY*distY);
+	}
     
     public void normalize() {
     // sets length to 1
