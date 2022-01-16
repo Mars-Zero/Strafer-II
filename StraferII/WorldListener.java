a595ba82e7727d0ec5e159064dfcab0f7d0c5f0c
Tudosie Razvan <razvanmariustudosie25@gmail.com>
2022-01-16 14:12:32 +0200

    #5 Some changes to the WorldListener

--- StraferII/WorldListener.java
+++ StraferII/WorldListener.java
@@ -5,12 +5,42 @@ public abstract class WorldListener extends Actor {
 
     Scroller scroller;
     Player player;
-
+    World world;
+    int change=1;
+    
     public WorldListener(World world) {
         scroller = world.getObjects(Scroller.class).get(0);
         player = world.getObjects(Player.class).get(0);
+        this.world=world;
     }
-
+    
+    
+    /**
+     * This method deletes all objects in the world
+     */
+    void deleteObjects(){
+        List<Actor> list=world.getObjects(Actor.class);
+        for(Actor actor:list)
+        {
+            world.removeObject(actor);
+        }
+        
+    }
+    
+    /**
+     * This method adds all objects that need to be put in the world
+     */
+    void addObjects(){
+        //add the player to it's position
+        //call different methods based on the world that needs to be changed
+        switch(change){
+          case 1:{
+              
+            break;
+        }  
+        };
+    }
+    
     public boolean isChangeWorld() {
 
         if (player.isAtEdge()) {
